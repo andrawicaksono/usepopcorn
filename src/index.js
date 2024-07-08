046c8741cde25a78c6adfa0bed9baa7d9f6f006c
andrawicaksono <andrawicaksono14@gmail.com>
2024-07-08 19:34:29 +0700

    add useeffect

--- src/index.js
+++ src/index.js
@@ -1,33 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
-
-function Test({ defaultRating = 0 }) {
-  const [movieRating, setMovieRating] = useState(defaultRating);
-
-  function handleSetRating(rating) {
-    setMovieRating(rating);
-  }
-
-  return (
-    <div>
-      <StarRating maxRating={10} color="blue" onSetRating={handleSetRating} />
-      <p>This movie was rated {movieRating} stars.</p>
-    </div>
-  );
-}
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating maxRating={5} color="red" defaultRating={3} size={24} />
-    <Test />
+    <App />
   </React.StrictMode>
 );
