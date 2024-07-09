78fd94cd8e8ed25e3173048a381e0707fc1ef17b
andrawicaksono <andrawicaksono14@gmail.com>
2024-07-09 16:39:55 +0700

    clean up data fetching

--- src/App.js
+++ src/App.js
@@ -63,13 +63,16 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) {
@@ -80,8 +83,11 @@ export default function App() {
 
           if (data.Response === "False") throw new Error("Movie not found!");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          console.log(err.message);
+
+          if (err.name !== "AbortError") setError(err.message);
         } finally {
           setIsLoading(false);
         }
@@ -94,6 +100,8 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return () => controller.abort();
     },
     [query]
   );
