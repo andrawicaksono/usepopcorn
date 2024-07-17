4b1c2ff75eb3240e5c0ee3063ac80d19e4834c1d
andrawicaksono <andrawicaksono14@gmail.com>
2024-07-17 14:40:01 +0700

    store to local storage

--- src/App-v2.js
+++ src/App-v2.js
@@ -55,15 +55,11 @@ const KEY = "383fc6c";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
-  // const [watched, setWatched] = useState([]);
-  const [watched, setWatched] = useState(function () {
-    const storedValue = localStorage.getItem("watched");
-    return JSON.parse(storedValue);
-  });
 
   useEffect(
     function () {
@@ -126,14 +122,6 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
-
-  useEffect(
-    function () {
-      localStorage.setItem("watched", JSON.stringify(watched));
-    },
-    [watched]
-  );
-
   return (
     <>
       <NavBar>
